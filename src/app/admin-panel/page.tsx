887682d029fcc64725eee274907b25acd57515f5
Abrar Ahmed <99709358+AbrarAhmed111@users.noreply.github.com>
2025-06-22 22:53:41 +0500

    Update page.tsx

--- src/app/admin-panel/page.tsx
+++ src/app/admin-panel/page.tsx
@@ -1,9 +1,6 @@
-import Dashboard from '@/NamePages/AdminSite/Dashboard'
+// app/admin-panel/page.tsx
+import { redirect } from 'next/navigation'
 
-export default function Home() {
-  return (
-    <div>
-      <Dashboard />
-    </div>
-  )
+export default function AdminPanel() {
+  redirect('/admin-panel/dashboard')
 }
